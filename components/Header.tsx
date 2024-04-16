8664355a77d7ef88612f3a92b939ac438d38aa24
Sharon <sharon.peishan.kennedy@gmail.com>
2024-04-15 23:09:24 -0400

    lint

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,24 +14,24 @@ const Header = () => {
   return (
     <header className="w-full">
       <div className="container mx-auto max-w-2xl px-4 sm:px-6 md:max-w-4xl lg:max-w-5xl xl:max-w-6xl xl:px-0">
-        <div className="flex w-full items-start sm:items-center justify-between py-5 sm:py-10">
+        <div className="flex w-full items-start justify-between py-5 sm:items-center sm:py-10">
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-end justify-between">
                 <div
                   id="main-logo"
-                  className="mr-4 sm:hidden text-3xl text-primary-500 dark:text-secondary-300"
+                  className="mr-4 text-3xl text-primary-500 dark:text-secondary-300 sm:hidden"
                 >
                   <Logo />
                 </div>
                 <div
                   id="main-logo"
-                  className="mr-4 hidden sm:block text-sm text-primary-500 dark:text-secondary-300"
+                  className="mr-4 hidden text-sm text-primary-500 dark:text-secondary-300 sm:block"
                 >
                   <Logomark />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden sm:block flex items-center font-logo text-4xl text-primary-500 dark:text-secondary-300 sm:text-5xl md:text-5xl">
+                  <div className="flex hidden items-center font-logo text-4xl text-primary-500 dark:text-secondary-300 sm:block sm:text-5xl md:text-5xl">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
